cca7a814075c33c8ff2c5fc04eb6dc8c5cbedc79
Joydey1800 <dey1800@gmail.com>
2020-08-12 22:36:28 +0600

    1

---
+++ public/index.js
@@ -0,0 +1,79 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+
+const myPeer = new Peer(undefined, {
+    secure: true,
+    host: 'joydey.herokuapp.com',
+    port: 443,
+});
+
+const peers = {};
+
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    // answer the call
+    myPeer.on('call', call => {
+        call.answer(stream);
+
+
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+
+    // when user connect
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    })
+
+
+    // when user disconnect
+    socket.on('user-disconnected', userId => {
+        if (peers[userId]) peers[userId].close()
+    })
+
+
+})
+
+
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+
+    videoGrid.append(video);
+}
+
+
+function connectToNewUser(userId, strem) {
+
+    const call = myPeer.call(userId, strem);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove();
+    })
+
+    peers[userId] = call;
+}
